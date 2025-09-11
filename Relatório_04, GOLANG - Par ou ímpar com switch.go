9b4f9746ffb90cf891ff682278ca56a179cef713
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2025-09-11 20:47:44 -0300

    Create Relatório_04, GOLANG - Par ou ímpar com switch.go

---
+++ Relatório_04, GOLANG - Par ou ímpar com switch.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+	fmt.Print("Digite um número: ") 
+	fmt.Scan(&num)
+	
+	switch num % 2{
+		case 0: fmt.Println("O número é par")
+		case 1: fmt.Println("O número é ímpar")
+	}
+}
